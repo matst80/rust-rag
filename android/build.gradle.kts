8920a3e6e6e181e228f97c23e1afd82b1418fff6
matst80 <mats.tornberg@gmail.com>
2026-05-23 15:43:30 +0200

    upgrade stuff

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.devtools.ksp") version "2.3.8" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
